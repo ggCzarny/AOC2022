a8b41eecc8b283f55efff1d51280fdcc9ac64bc8
Michal <mucios91@gmail.com>
2022-12-23 18:08:47 +0100

    d4 done

---
+++ 04/d4p1.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+def main():
+    list = np.arange(0, 100000, 1)
+    count = 0
+    with open("d4.txt") as f:
+        for line in f.readlines():
+            line = line.replace('\n', '')
+            fs = int(line.split(',')[0].split('-')[0]) #first start 
+            fe = int(line.split(',')[0].split('-')[1]) #first start 
+
+            ss = int(line.split(',')[1].split('-')[0]) #first start 
+            se = int(line.split(',')[1].split('-')[1]) #first start 
+
+            list1 = list[fs:fe+1]
+            list2 = list[ss:se+1]
+            if set(list1).issubset(list2) or set(list2).issubset(list1):
+                count += 1
+        f.close()
+        print(count)
+
+if __name__ == "__main__":
+    main()
