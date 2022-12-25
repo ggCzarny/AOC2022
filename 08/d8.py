8dd9b4de9c0b0f2f5e8dc69092557a7847ba6399
Michal <mucios91@gmail.com>
2022-12-25 17:51:01 +0100

    łatwo

--- 08/d8.py
+++ 08/d8.py
@@ -1,4 +1,4 @@
-class main():
+class Main:
     def __init__(self, test=False):
         TEST = test
         if TEST:
@@ -65,9 +65,38 @@ class main():
                 print(x)
         print(f'Numer of visible trees: {count}')
 
+    def find_highest_scenic_score(self):
+        size = len(self.array)
+        max_score = 0
+
+        def count_view_distance(view_list, element):
+            if len(view_list) == 0:
+                return 0
+            output = [view_list.index(i) for i in view_list if i >= element]
+            if len(output) == 0:
+                return len(view_list)
+            return  output[0] + 1
+
+        for x in range(size):
+            for y in range(size):
+                element = int(self.array[x][y])
+                element_column = [int(i[y] )for i in self.array]
+                element_row = [int(i) for i in self.array[x]]
+                view_right = element_row[y+1:]
+                view_left = element_row[:y]
+                view_left.reverse()
+                view_up = element_column[:x]
+                view_up.reverse()
+                view_down = element_column[x+1:]
+                
+                score = count_view_distance(view_left, element) * count_view_distance(view_right, element) * count_view_distance(view_up, element) * count_view_distance(view_down, element)
+                if max_score < score:
+                    max_score = score
+        print(f'Max Score: {max_score}')
+
 if __name__ == "__main__":
-    main(test=False).print_array()
-    print()
-    main(test=False).find_uncovered()
+    lesgo = Main(test=False)
+    lesgo.print_array()
+    lesgo.find_highest_scenic_score()
 
 
